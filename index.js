3d357e173d79c1fa1fd034621cb6d5f1f274edee
klaraassvensson <klara.svensson04@gmail.com>
2024-12-28 20:16:09 +0100

    första försöket att placera spelarens bricka
    just nu placerar de sig inte rätt, lägger utifrån raderna istället för kolumnerna

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const h1DOM = document.querySelector("h1");
-const connect4Squre = document.getElementById("connect4");
+const boardDOM = document.getElementById("board");
 const nCols = 7;
 const nRows  = 6;
+let currentPlayer = 1;
 
 function createCircles (){
     for (let i = 0; i < nCols; i++){
         for (let j = 0; j < nRows; j++){
             const circleDiv = document.createElement("div");
-            connect4Squre.appendChild(circleDiv);
+            boardDOM.appendChild(circleDiv);
             circleDiv.classList.add("circle"); 
-            circleDiv.setAttribute("id", [i, j])   
+            circleDiv.setAttribute("id", i + "," + j)   
         }
     }
 }
 
 createCircles()
+
+const circles = document.querySelectorAll(".circle");
+
+boardDOM.addEventListener("click", (e) => {
+    const clickedCircle = e.target;
+    if (clickedCircle.classList.contains("circle")){
+        const colIndex = parseInt(clickedCircle.id.match(/^(\d+),(\d+)$/)[0]);
+        for (let rowIndex = nRows - 1; rowIndex >= 0; rowIndex--){
+            const circle = document.getElementById(`${colIndex},${rowIndex}`);
+            if (!circle.classList.contains("player-one") && !circle.classList.contains("player-two")){
+                if (currentPlayer == 1){
+                    circle.classList.add("player-one");
+                    currentPlayer = 2;
+                } else {
+                    circle.classList.add("player-two");
+                    currentPlayer = 1;
+                }
+                break;
+            }
+        }
+    }
+})
